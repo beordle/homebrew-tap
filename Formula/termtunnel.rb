e272527e8fa5ea577d1d70540281c5f82a64e500
Jindong Zhang <beordle@gmail.com>
2022-05-01 20:21:38 +0800

    tap desc fix

--- Formula/termtunnel.rb
+++ Formula/termtunnel.rb
@@ -1,6 +1,6 @@
 class Termtunnel < Formula
-  desc "a terminal tunnel tool like lrzsz"
-  homepage ""
+  desc "Terminal tunnel tool like lrzsz"
+  homepage "https://github.com/beordle/termtunnel"
   url "https://github.com/beordle/termtunnel/archive/refs/tags/v1.4.tar.gz"
   sha256 "d35b97659b2e7357c4ce78767ac31297639615caa29de862099ea5bac4938976"
   license "MIT"
@@ -10,7 +10,6 @@ class Termtunnel < Formula
     system "make"
     bin.install "termtunnel"
   end
-
 
   test do
     system "#{bin}/termtunnel"
