f97370a12bb99e4fd12c1657791446c52185ea09
Jindong Zhang <beordle@gmail.com>
2022-05-01 20:13:08 +0800

    add termtunnel

---
+++ Formula/termtunnel.rb
@@ -0,0 +1,17 @@
+class Termtunnel < Formula
+  desc "Termtunnel is a terminal tunnel tool like lrzsz"
+  homepage ""
+  url "https://github.com/beordle/termtunnel/archive/refs/tags/v1.4.tar.gz"
+  sha256 "d35b97659b2e7357c4ce78767ac31297639615caa29de862099ea5bac4938976"
+  license "MIT"
+  depends_on "cmake" => :build
+  def install
+    system "cmake", "."
+    system "make"
+    bin.install "termtunnel"
+  end
+
+  test do
+    system "#{bin}/termtunnel"
+  end
+end
