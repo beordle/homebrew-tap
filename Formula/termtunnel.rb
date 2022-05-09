42182bda779d05d968e58eb940e5d99dcae76802
Jindong Zhang <beordle@gmail.com>
2022-05-09 14:14:42 +0800

    modify desc

--- Formula/termtunnel.rb
+++ Formula/termtunnel.rb
@@ -1,5 +1,5 @@
 class Termtunnel < Formula
-  desc "Termtunnel tunnel tool over tty."
+  desc "Terminal tunnel tool over tty."
   homepage "https://github.com/beordle/termtunnel"
   url "https://github.com/beordle/termtunnel/archive/refs/tags/version-1.5.4.tar.gz"
   sha256 "bce2574a5762329cefda363136b1ae69b220d0047f805a8ea31ee61ec8fec704"
