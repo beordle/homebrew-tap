3390ce75ef7411972a4a23421abf5740b731a42c
Jindong Zhang <beordle@gmail.com>
2022-05-15 12:10:50 +0800

    termtunnel 1.7.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/termtunnel.rb
+++ Formula/termtunnel.rb
@@ -1,8 +1,8 @@
 class Termtunnel < Formula
   desc "Cross-platform terminal tunnel tool"
   homepage "https://github.com/beordle/termtunnel"
-  url "https://github.com/beordle/termtunnel/archive/refs/tags/version-1.6.4.tar.gz"
-  sha256 "35be0f2dc4fa5a76da2bd731c9aae3801342b094ed6df1dcd7fbde6e67c5a8c5"
+  url "https://github.com/beordle/termtunnel/archive/refs/tags/version-1.7.0.tar.gz"
+  sha256 "f58ff13f8defeb5a4c1fc13af15cc2624d971cd1f2e108bc8fa483fcca5d856c"
   license "MIT"
   depends_on "cmake" => :build
   def install
